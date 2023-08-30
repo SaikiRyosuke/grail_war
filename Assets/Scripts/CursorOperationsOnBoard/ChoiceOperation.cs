4fcf535fea3e374b21545f9bd11dd43bb2fe2d95
DESKTOP-CES5PO4\saiki ryosuke <ryosuke.saiki@gmail.com>
2023-08-31 02:08:57 +0900

    新しいスキル用のボタンプレファブを用意し、それが押された時、GeneralUnitにそれを伝えるメソッドを作りました。

--- Assets/Scripts/CursorOperationsOnBoard/ChoiceOperation.cs
+++ Assets/Scripts/CursorOperationsOnBoard/ChoiceOperation.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ChoiceOperation : MonoBehaviour
 {
@@ -11,10 +12,13 @@ public class ChoiceOperation : MonoBehaviour
     [SerializeField] GameObject unitChoiceUI;
     [SerializeField] GameObject buttonPrefab;
     [SerializeField] Camera mainCamera;
+    [SerializeField] DataBoardManager dataBoardManager;
 
     const int UNIT_CHOICE_MAX = 5;
-    const float RADIUS = 90f;
+    const float RADIUS = 1f;
     float BUTTON_SCALE;
+	UnitGeneral selectedUnit;
+	
 
     public void Activate(Vector2Int unitPosition)
     {
@@ -22,19 +26,19 @@ public class ChoiceOperation : MonoBehaviour
         sceneManager.operationType = SceneManager.OperationType.Choice;
 
         //TODOスケールの変換
-        //100はボタンのもとのサイズ
-        BUTTON_SCALE = 2 * RADIUS * Mathf.Sin(Mathf.PI / ((UNIT_CHOICE_MAX - 1) * 2)) / 100;
+        //ここの係数は本画像が用意できたときに改めて変更しよう
+        BUTTON_SCALE = 2 * RADIUS * Mathf.Sin(Mathf.PI / ((UNIT_CHOICE_MAX - 1) * 2))/4;
 
         Debug.Log(Methods.BoardToUnitVector(unitPosition));
         //ボード座標をUIスクリーン座標に変換
-        unitChoiceUI.transform.position = mainCamera.WorldToScreenPoint(Methods.BoardToUnitVector(unitPosition));
+        // unitChoiceUI.transform.position = mainCamera.WorldToScreenPoint(Methods.BoardToUnitVector(unitPosition));
 		//for分にしておこう
 		//unitChoiceUIはプレファブ
-        InstantiateUnitChoiceButton(unitChoiceUI, "hello", 0);
-        InstantiateUnitChoiceButton(unitChoiceUI, "hello", 1);
-        InstantiateUnitChoiceButton(unitChoiceUI, "hello", 2);
-        InstantiateUnitChoiceButton(unitChoiceUI, "hello", 3);
-        InstantiateUnitChoiceButton(unitChoiceUI, "hello", 4);
+		//これは嘘。Canvasの子オブジェクト。これを自分自身に変更してUnitの子オブジェクトとしてボタンを生成する。
+		selectedUnit = dataBoardManager.GetFromDataBoard(unitPosition);
+		for(int i = 0; i < UNIT_CHOICE_MAX; i++){
+			InstantiateUnitChoiceButton(selectedUnit.gameObject, i);
+		}
     }
 
 
@@ -53,22 +57,39 @@ public class ChoiceOperation : MonoBehaviour
         }
     }
 
-    public void InstantiateUnitChoiceButton(GameObject parentObject, string name, int order)
+    public void InstantiateUnitChoiceButton(GameObject parentObject, int order)
     {
         GameObject button = Instantiate(this.buttonPrefab, OrderToPosition(order) , Quaternion.identity);
 
-        button.name = name;
+        button.name = selectedUnit.gameObject.name + "skill_" + order.ToString();
         button.transform.SetParent(parentObject.transform, false);
         button.transform.localScale = Vector3.one * BUTTON_SCALE;
-
-
+		
+		EventTrigger trigger = button.GetComponent<EventTrigger>();
+		if(trigger == null){
+			Debug.Log("EventTrigger　is not attached to skill button");
+			return;
+		}
+		EventTrigger.Entry  entry = new EventTrigger.Entry();
+		entry.eventID = EventTriggerType.PointerClick;
+		
+		entry.callback.AddListener(_ => Skill(order));
+		trigger.triggers.Add(entry);
     }
+	
+	
+	public void Skill(int order)
+	{
+		//order番目のスキルを使用するようGeneralUnitに指示する。
+		Debug.Log(selectedUnit.gameObject.name + "の" + "skill_" + order.ToString() + "を発動！");
+	}
 
     public Vector2 OrderToPosition(int order)
     {
         float angle = UNIT_CHOICE_MAX == 1 ? 0 :  -Mathf.PI * (float)order / ((float) UNIT_CHOICE_MAX - 1); 
         //magic number y座標の調整
-        return new Vector2(RADIUS * Mathf.Cos(angle), RADIUS * Mathf.Sin(angle) + 20);
+        return new Vector2(RADIUS * Mathf.Cos(angle), RADIUS * Mathf.Sin(angle));
+		//ｙ座標の+20を消しました。
     }
     void EndChoice()
     {
